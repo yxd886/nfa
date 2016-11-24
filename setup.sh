7f8d22ca0fb39a4207fedca83f46107704febff6
djp <duanjp8617@gmail.com>
2016-11-24 21:22:23 +0800

    test setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+# record current working directory
+PWD=`pwd`
+
 # switch to the deps directory
 cd deps
 
@@ -9,3 +12,12 @@ git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
 make -j`nproc`
+sudo make install
+
+# go to /third_party/protobuf and install protobuf
+cd ./third_party/protobuf
+make -j`nproc`
+sudo make install
+
+# switch to nfa directory
+cd $PWD
