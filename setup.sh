c9318eb1722f3728a5f088d28e612662b8f57e03
djp <duanjp8617@gmail.com>
2016-11-24 21:48:34 +0800

    test setup.sh

--- setup.sh
+++ setup.sh
@@ -6,18 +6,24 @@ PWD=`pwd`
 # switch to the deps directory
 cd deps
 
-# start installing grpc
-sudo apt-get install build-essential autoconf libtool
-git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
-cd grpc
-git submodule update --init
-make -j`nproc`
-sudo make install
+# install grpc and protocbuf
+if [ ! -d "./grpc" ]; then
 
-# go to /third_party/protobuf and install protobuf
-cd ./third_party/protobuf
-make -j`nproc`
-sudo make install
+  # build and install grpc
+  sudo apt-get install build-essential autoconf libtool
+  git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
+  cd grpc
+  git submodule update --init
+  make -j`nproc`
+  sudo make install
+
+  # go to /third_party/protobuf and install protobuf
+  cd ./third_party/protobuf
+  make -j`nproc`
+  sudo make install
+else
+  echo "grpc and protobuf have already been installed."
+fi
 
 # switch to nfa directory
 cd $PWD
