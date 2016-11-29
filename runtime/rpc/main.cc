d2da30b24c51f195a624ab671d11842d8bb9f07b
xiaodongyi <xiaodongyi@Admins-iMac.local>
2016-11-29 17:39:18 +0800

    ok111111111111222221

--- runtime/rpc/main.cc
+++ runtime/rpc/main.cc
@@ -129,8 +129,8 @@ int main(int argc, char** argv) {
   std::string reply = greeter.SayHello(user);  // The actual RPC call!
   std::cout << "Greeter received: " << reply << std::endl;
 
-  reply = greeter.SayHelloagain(user);  // The actual RPC call!
-  std::cout << "Greeter received: " << reply << std::endl;
+  //reply = greeter.SayHelloagain(user);  // The actual RPC call!
+  //std::cout << "Greeter received: " << reply << std::endl;
 
   return 0;
 }
