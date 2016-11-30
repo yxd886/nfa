f6f96a8ac197c8645e1ae674d4d88ebc93943aaf
xiaodongyi <xiaodongyi@Admins-iMac.local>
2016-11-30 13:11:07 +0800

    ok121212121212121112212345

--- runtime/rpc/nfa_rpc_server.cc
+++ runtime/rpc/nfa_rpc_server.cc
@@ -311,7 +311,7 @@ int main(int argc, char** argv) {
 
 	  std::vector<struct vswitch_msg>::iterator iter;
 	  while(1){
-		  sleep(0.5);
+		  sleep(1.0);
 	   mtx.lock();
 	   std::cout<<"get the lock to find request"<<std::endl;
    	   for(iter=rte_ring.begin();iter!=rte_ring.end();iter++){
