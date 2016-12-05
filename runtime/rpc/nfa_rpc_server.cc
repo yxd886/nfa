c7a412e72cb7031de87c1c590e874ffedb66489a
xiaodongyi <xiaodongyi@Admins-iMac.local>
2016-12-05 10:40:04 +0800

    extract the class3aa

--- runtime/rpc/nfa_rpc_server.cc
+++ runtime/rpc/nfa_rpc_server.cc
@@ -73,12 +73,9 @@ using nfa_msg::RuntimeStatRequest;
 #include "nfa_rpc_server.h"
 
 
-class LivenessCheck {
-public:
-	// Take in the "service" instance (in this case representing an asynchronous
-	// server) and the completion queue "cq" used for asynchronous communication
-	// with the gRPC runtime.
-	LivenessCheck(Runtime_RPC::AsyncService* service, ServerCompletionQueue* cq,std::map< int, struct Local_view> *viewlist_input,std::map< int, struct Local_view> *viewlist_output, moodycamel::ConcurrentQueue<struct request_msg> *rte_ring_request,moodycamel::ConcurrentQueue<struct reply_msg> *rte_ring_reply,int worker_id)
+
+
+LivenessCheck::LivenessCheck(Runtime_RPC::AsyncService* service, ServerCompletionQueue* cq,std::map< int, struct Local_view> *viewlist_input,std::map< int, struct Local_view> *viewlist_output, moodycamel::ConcurrentQueue<struct request_msg> *rte_ring_request,moodycamel::ConcurrentQueue<struct reply_msg> *rte_ring_reply,int worker_id)
 		: service_(service), cq_(cq), responder_(&ctx_), status_(CREATE),worker_id(worker_id),viewlist_input(viewlist_input),viewlist_output(viewlist_output),rte_ring_request(rte_ring_request),rte_ring_reply(rte_ring_reply){
 		// Invoke the serving logic right away.
 		tags.index=LIVENESSCHECK;
@@ -86,7 +83,7 @@ public:
 		Proceed();
 	}
 
-	void Proceed() {
+LivenessCheck::Proceed() {
 		if (status_ == CREATE) {
 			status_ = PROCESS;
 			service_->RequestLivenessCheck(&ctx_, &request_, &responder_, cq_, cq_,
@@ -102,27 +99,8 @@ public:
 			delete this;
 		}
 	}
-private:
 
-	Runtime_RPC::AsyncService* service_;
-	ServerCompletionQueue* cq_;
-	ServerContext ctx_;
-	LivenessRequest request_;
-	LivenessReply reply_;
 
-	// The means to get back to the client.
-	ServerAsyncResponseWriter<LivenessReply> responder_;
-
-	// Let's implement a tiny state machine with the following states.
-	enum CallStatus { CREATE, PROCESS, FINISH };
-	CallStatus status_;  // The current serving state.
-	struct tag tags;
-	std::map< int, struct Local_view> *viewlist_input;
-	std::map< int, struct Local_view> *viewlist_output;
-	moodycamel::ConcurrentQueue<struct request_msg> *rte_ring_request;
-	moodycamel::ConcurrentQueue<struct reply_msg> *rte_ring_reply;
-	int worker_id;
-};
 
 class AddInputView {
 public:
@@ -1494,6 +1472,67 @@ int parse_mac_addr(char *addr, const char *str ){
 	}
 
 	return 0;
+}
+
+int encode_mac_addr(char *str, char *addr){
+	if (str != NULL && addr != NULL) {
+		int r = sprintf(str,
+			       "%2hhx:%2hhx:%2hhx:%2hhx:%2hhx:%2hhx",
+			       addr,
+			       addr+1,
+			       addr+2,
+			       addr+3,
+			       addr+4,
+			       addr+5);
+
+		if (r != 6)
+			return -EINVAL;
+	}
+
+	return 0;
+}
+
+int parse_ip_addr(char *addr, const char *str ){
+	if (str != NULL && addr != NULL) {
+		strcpy(addr,str);
+	}
+
+	return 0;
+}
+
+
+
+int encode_ip_addr( char *str ,char *addr )
+{
+	if (str != NULL && addr != NULL) {
+		strcpy(str,addr);
+	}
+
+	return 0;
+}
+
+
+void view_rpc2local(Local_view* local_view, View source){
+	local_view->worker_id=source.worker_id();
+	parse_mac_addr(local_view->control_port_mac,source.control_port_mac().c_str());
+	parse_mac_addr(local_view->input_port_mac,source.input_port_mac().c_str());
+	parse_mac_addr(local_view->output_port_mac,source.output_port_mac().c_str());
+	parse_ip_addr(local_view->rpc_ip,source.rpc_ip().c_str());
+	local_view->rpc_port=source.rpc_port();
+	}
+
+void view_local2rpc(View* rpc_view_ptr, Local_view local_view ){
+	char str_tmp[20];
+	rpc_view_ptr->set_worker_id(local_view.worker_id);
+	encode_mac_addr(str_tmp,local_view.control_port_mac);
+	rpc_view_ptr->set_control_port_mac(str_tmp);
+	encode_mac_addr(str_tmp,local_view.input_port_mac);
+	rpc_view_ptr->set_input_port_mac(str_tmp);
+	encode_mac_addr(str_tmp,local_view.output_port_mac);
+	rpc_view_ptr->set_output_port_mac(str_tmp);
+	encode_ip_addr(str_tmp,local_view.rpc_ip);
+	rpc_view_ptr->set_rpc_ip(str_tmp);
+	rpc_view_ptr->set_rpc_port(local_view.rpc_port);
 }
 
 int main(int argc, char** argv) {
--- runtime/rpc/nfa_rpc_server.h
+++ runtime/rpc/nfa_rpc_server.h
@@ -112,66 +112,47 @@ struct tag{
 
 };
 
-
 int parse_mac_addr(char *addr, const char *str );
-static int encode_mac_addr(char *str, char *addr  )
-{
-	if (str != NULL && addr != NULL) {
-		int r = sprintf(str,
-			       "%2hhx:%2hhx:%2hhx:%2hhx:%2hhx:%2hhx",
-			       addr,
-			       addr+1,
-			       addr+2,
-			       addr+3,
-			       addr+4,
-			       addr+5);
+int encode_mac_addr(char *str, char *addr);
+int parse_ip_addr(char *addr, const char *str );
+int encode_ip_addr( char *str ,char *addr );
 
-		if (r != 6)
-			return -EINVAL;
-	}
+void view_rpc2local(Local_view* local_view, View source);
+void view_local2rpc(View* rpc_view_ptr, Local_view local_view );
 
-	return 0;
-}
 
-static int parse_ip_addr(char *addr, const char *str )
-{
-	if (str != NULL && addr != NULL) {
-		strcpy(addr,str);
-	}
 
-	return 0;
-}
 
-static int encode_ip_addr( char *str ,char *addr )
-{
-	if (str != NULL && addr != NULL) {
-		strcpy(str,addr);
-	}
+class LivenessCheck {
+public:
+	// Take in the "service" instance (in this case representing an asynchronous
+	// server) and the completion queue "cq" used for asynchronous communication
+	// with the gRPC runtime.
+	LivenessCheck(Runtime_RPC::AsyncService* service, ServerCompletionQueue* cq,std::map< int, struct Local_view> *viewlist_input,std::map< int, struct Local_view> *viewlist_output, moodycamel::ConcurrentQueue<struct request_msg> *rte_ring_request,moodycamel::ConcurrentQueue<struct reply_msg> *rte_ring_reply,int worker_id);
+	void Proceed();
 
-	return 0;
-}
+private:
 
-static void view_rpc2local(Local_view* local_view, View source){
-	local_view->worker_id=source.worker_id();
-	parse_mac_addr(local_view->control_port_mac,source.control_port_mac().c_str());
-	parse_mac_addr(local_view->input_port_mac,source.input_port_mac().c_str());
-	parse_mac_addr(local_view->output_port_mac,source.output_port_mac().c_str());
-	parse_ip_addr(local_view->rpc_ip,source.rpc_ip().c_str());
-	local_view->rpc_port=source.rpc_port();
-	}
-static void view_local2rpc(View* rpc_view_ptr, Local_view local_view ){
-	char str_tmp[20];
-	rpc_view_ptr->set_worker_id(local_view.worker_id);
-	encode_mac_addr(str_tmp,local_view.control_port_mac);
-	rpc_view_ptr->set_control_port_mac(str_tmp);
-	encode_mac_addr(str_tmp,local_view.input_port_mac);
-	rpc_view_ptr->set_input_port_mac(str_tmp);
-	encode_mac_addr(str_tmp,local_view.output_port_mac);
-	rpc_view_ptr->set_output_port_mac(str_tmp);
-	encode_ip_addr(str_tmp,local_view.rpc_ip);
-	rpc_view_ptr->set_rpc_ip(str_tmp);
-	rpc_view_ptr->set_rpc_port(local_view.rpc_port);
-}
+	Runtime_RPC::AsyncService* service_;
+	ServerCompletionQueue* cq_;
+	ServerContext ctx_;
+	LivenessRequest request_;
+	LivenessReply reply_;
+
+	// The means to get back to the client.
+	ServerAsyncResponseWriter<LivenessReply> responder_;
+
+	// Let's implement a tiny state machine with the following states.
+	enum CallStatus { CREATE, PROCESS, FINISH };
+	CallStatus status_;  // The current serving state.
+	struct tag tags;
+	std::map< int, struct Local_view> *viewlist_input;
+	std::map< int, struct Local_view> *viewlist_output;
+	moodycamel::ConcurrentQueue<struct request_msg> *rte_ring_request;
+	moodycamel::ConcurrentQueue<struct reply_msg> *rte_ring_reply;
+	int worker_id;
+};
+
 
 
 
