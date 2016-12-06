a820fea45a79df0a79bb5b123f05daae4a521428
djp <duanjp8617@gmail.com>
2016-12-06 11:54:15 +0800

    c

--- runtime/nfaflags.cc
+++ runtime/nfaflags.cc
@@ -7,8 +7,9 @@ using namespace std;
 
 DEFINE_bool(boolean_flag, true, "a test boolean flag");
 
-static bool ValidatePort(const char* flagname, string value) {
-   if (value == "wtf")   // value is ok
+static bool ValidatePort(const char* flagname, const char* str_arg) {
+  string value(str_arg);
+  if (value == "wtf")   // value is ok
      return true;
    printf("Invalid value for --%s: %s\n", flagname, value.c_str());
    return false;
