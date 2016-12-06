311c7c6a52bba94ec445e58ea9c0f8646d4278d0
djp <duanjp8617@gmail.com>
2016-12-06 11:51:51 +0800

    c

--- runtime/nfaflags.cc
+++ runtime/nfaflags.cc
@@ -1,4 +1,17 @@
+#include <iostream>
+#include <string>
+
 #include "nfaflags.h"
 
+using namespace std;
+
 DEFINE_bool(boolean_flag, true, "a test boolean flag");
+
+static bool ValidatePort(const char* flagname, string value) {
+   if (value == "wtf")   // value is ok
+     return true;
+   printf("Invalid value for --%s: %s\n", flagname, value.c_str());
+   return false;
+}
 DEFINE_string(string_flag, "a,b,c", "comma spearated string flag");
+DEFINE_validator(string_flag, &ValidatePort);
