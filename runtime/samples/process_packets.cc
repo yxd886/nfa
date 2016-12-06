d57082412103ae3e5bfecab651346b061bd4ef77
djp <duanjp8617@gmail.com>
2016-12-06 12:42:21 +0800

    c

--- runtime/samples/process_packets.cc
+++ runtime/samples/process_packets.cc
@@ -9,10 +9,16 @@ using namespace std;
 int main(int argc, char* argv[]){
   google::ParseCommandLineFlags(&argc, &argv, true);
 
-  google::InitGoogleLogging(argv[0]);
-
-  LOG(INFO) << "Found " << 100 << " cookies";
-
   cout<<FLAGS_boolean_flag<<endl;
   cout<<FLAGS_string_flag<<endl;
+
+  LOG(INFO) << "file";
+  // Most flags work immediately after updating values.
+  FLAGS_logtostderr = 1;
+  LOG(INFO) << "stderr";
+  FLAGS_logtostderr = 0;
+  // This won't change the log destination. If you want to set this
+  // value, you should do this before google::InitGoogleLogging .
+  FLAGS_log_dir = "./";
+  LOG(INFO) << "the same file";
 }
