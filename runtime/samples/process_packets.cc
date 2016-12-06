056e00a5f3075f9b2ace9fb940107d447affb7c5
djp <duanjp8617@gmail.com>
2016-12-06 13:50:38 +0800

    c

--- runtime/samples/process_packets.cc
+++ runtime/samples/process_packets.cc
@@ -2,13 +2,36 @@
 #include <cstdio>
 
 #include <rte_config.h>
+#include <rte_config.h>
+#include <rte_cycles.h>
 #include <rte_eal.h>
+#include <rte_ethdev.h>
 
 #include <glog/logging.h>
 
 #include "../nfaflags.h"
 
 using namespace std;
+
+static void disable_syslog() {
+  setlogmask(0x01);
+}
+
+static void enable_syslog() {
+  setlogmask(0xff);
+}
+
+static ssize_t dpdk_log_init_writer(void *, const char *data, size_t len) {
+  enable_syslog();
+  LOG(INFO) << std::string(data, len);
+  disable_syslog();
+  return len;
+}
+
+static ssize_t dpdk_log_writer(void *, const char *data, size_t len) {
+  LOG(INFO) << std::string(data, len);
+  return len;
+}
 
 int main(int argc, char* argv[]){
   google::ParseCommandLineFlags(&argc, &argv, true);
@@ -18,6 +41,11 @@ int main(int argc, char* argv[]){
 
   FLAGS_logtostderr = 1;
   google::InitGoogleLogging(argv[0]);
+
+  int ret;
+  int i;
+
+  FILE *org_stdout;
 
   int rte_argc = 0;
   const char *rte_argv[16];
@@ -35,16 +63,36 @@ int main(int argc, char* argv[]){
   rte_argv[rte_argc++] = opt_lcore_bitmap;
   rte_argv[rte_argc++] = "-n";
   rte_argv[rte_argc++] = "4";
-  // rte_argv[rte_argc++] = "--proc-type";
-  // rte_argv[rte_argc++] = "secondary";
+  rte_argv[rte_argc++] = "--proc-type";
+  rte_argv[rte_argc++] = "secondary";
   rte_argv[rte_argc] = nullptr;
 
-  int ret = rte_eal_init(rte_argc, const_cast<char **>(rte_argv));
+  cookie_io_functions_t dpdk_log_init_funcs;
+  cookie_io_functions_t dpdk_log_funcs;
+
+  std::memset(&dpdk_log_init_funcs, 0, sizeof(dpdk_log_init_funcs));
+  std::memset(&dpdk_log_funcs, 0, sizeof(dpdk_log_funcs));
+
+  dpdk_log_init_funcs.write = &dpdk_log_init_writer;
+  dpdk_log_funcs.write = &dpdk_log_writer;
+
+  org_stdout = stdout;
+  stdout = fopencookie(nullptr, "w", dpdk_log_init_funcs);
+
+  disable_syslog();
+
+  ret = rte_eal_init(rte_argc, const_cast<char **>(rte_argv));
 
   if (ret < 0) {
     LOG(ERROR) << "rte_eal_init() failed: ret = " << ret;
     exit(EXIT_FAILURE);
   }
+
+  enable_syslog();
+  fclose(stdout);
+  stdout = org_stdout;
+
+  rte_openlog_stream(fopencookie(nullptr, "w", dpdk_log_funcs));
 
   LOG(INFO) << "DPDK EAL finishes initialization";
 }
