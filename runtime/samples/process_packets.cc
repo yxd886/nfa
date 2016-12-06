612ebec55a0fd85c82cfcdb793e708ccd844a1f7
djp <duanjp8617@gmail.com>
2016-12-06 12:45:16 +0800

    c

--- runtime/samples/process_packets.cc
+++ runtime/samples/process_packets.cc
@@ -12,15 +12,10 @@ int main(int argc, char* argv[]){
   cout<<FLAGS_boolean_flag<<endl;
   cout<<FLAGS_string_flag<<endl;
 
+  FLAGS_log_dir = "/some/log/directory";
   google::InitGoogleLogging(argv[0]);
-
   LOG(INFO) << "file";
-   // Most flags work immediately after updating values.
-   FLAGS_logtostderr = 1;
+
    LOG(INFO) << "stderr";
-   FLAGS_logtostderr = 0;
-   // This won't change the log destination. If you want to set this
-   // value, you should do this before google::InitGoogleLogging .
-   FLAGS_log_dir = "/some/log/directory";
    LOG(INFO) << "the same file";
 }
