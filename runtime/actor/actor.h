413f33302139efb8f10d54ceb7f69f9d9dcbcb51
xiaodongyi <xiaodongyi@Admins-iMac.local>
2016-12-09 15:14:29 +0800

    actor

--- runtime/actor/actor.h
+++ runtime/actor/actor.h
@@ -45,8 +45,8 @@ public:
 		dst_actor.handle_message(x,y,z);
 	}
 	template <typename T,typename U,typename V,typename W>
-	void local_send(actor& dst_actor,T x,U y,V z,{
-		dst_actor.handle_message(x,y,z);
+	void local_send(actor& dst_actor,T x,U y,V z,W w){
+		dst_actor.handle_message(x,y,z,w);
 	}
 
 	void remote_send(int runtime_id,int actor_id,char* msg,int size);
@@ -56,11 +56,12 @@ public:
   virtual void handle_message(struct prepare_to_get_replica*){}
   virtual void handle_message(struct get_the_fking_replica*){}
   virtual void handle_message(struct rep_peer_fail*){}
-  virtual void handle_message(struct rep_peer_back_to_alive*,const actor& new_replication_target_a){}
-  virtual void handle_message(struct clean_up_vswitch_table*,int arg_to_rt_id){}
+  virtual void handle_message(struct rep_peer_back_to_alive*,const actor& ){}
+  virtual void handle_message(struct clean_up_vswitch_table*,int ){}
   virtual void handle_message(struct change_route_atom*){}
   virtual void handle_message(struct get_vswitch_atom*){}
-  virtual void handle_message(int new_replication_target_rt_id, const actor& new_replication_target_a){}
+  virtual void handle_message(int , const actor& ){}
+  virtual void handle_message(struct request_replication_target*, int , int){}
 
 private:
 	int actor_id;
