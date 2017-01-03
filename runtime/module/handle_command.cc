be03799316d2f4339c788eeb115d10032e976f59
xiaodongyi <xiaodongyi@Admins-iMac.local>
2017-01-03 20:33:42 +0800

    test msg

--- runtime/module/handle_command.cc
+++ runtime/module/handle_command.cc
@@ -58,6 +58,7 @@ struct task_result handle_command::RunTask(void *arg){
 
         reliable_p2p& r = coordinator_actor_->reliables_.find(item->rt_config.runtime_id)->second;
         coordinator_actor_->mac_to_reliables_.emplace(item->rt_config.runtime_id, r);
+        r.reliable_send(0,1,1,es_scheduler_pkt_batch_t::value,nullptr);
         break;
       }
       case rpc_operation::delete_input_runtime :{
