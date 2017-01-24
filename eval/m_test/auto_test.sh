52ec09eb88e7dd724a1c6bfa605e605122e9cbcd
xiaodong yi <409070169@qq.com>
2017-01-24 15:43:04 +0800

    small modi

--- eval/m_test/auto_test.sh
+++ eval/m_test/auto_test.sh
@@ -2,10 +2,6 @@
 
 ulimit -Hn 70000
 ulimit -Sn 70000
-
-./tp_eval.py --r1=6 --r2=3 --r3=2 --sc=firewall,flow_monitor,http_parser
-./tp_eval.py --r1=6 --r2=3 --r3=3 --sc=firewall,flow_monitor,http_parser
-
 ./tp_eval.py --r1=6 --r2=1 --r3=0 --sc=flow_monitor
 ./tp_eval.py --r1=6 --r2=1 --r3=1 --sc=flow_monitor
 ./tp_eval.py --r1=6 --r2=2 --r3=1 --sc=flow_monitor
