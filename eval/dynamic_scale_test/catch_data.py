461f6341b0a014cf8205202d7031a91d8be247a6
xiaodongyi <xiaodongyi@Admins-iMac.local>
2017-01-27 12:29:54 +0800

    a

--- eval/dynamic_scale_test/catch_data.py
+++ eval/dynamic_scale_test/catch_data.py
@@ -177,7 +177,17 @@ def test():
   return (after_received-before_received)/3, after_dropped-before_dropped, after_time-before_time
 
 
-def local_test():
+def seperate_test():
+
+  ssh_r2 = paramiko.SSHClient()
+  ssh_r2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  ssh_r2.connect('202.45.128.155',username='net',password='netexplo')
+  ssh_r2.exec_command('cd ~/nfa/eval/r_test')
+
+  ssh_r3 = paramiko.SSHClient()
+  ssh_r3.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  ssh_r3.connect('202.45.128.156',username='net',password='netexplo')
+  ssh_r3.exec_command('cd ~/nfa/eval/r_test')
 
   recovery_time = "";
   migration_time = "";
@@ -191,64 +201,116 @@ def local_test():
 #  if options.test_type == "THROUGHPUT":
 #  print "Start Testing Throughput"
 
-  tmp1,tmp2 = local_read_pkts(1)
-  before_received +=tmp1;
-  before_dropped +=tmp2;
+  beforer21tmp1,beforer21tmp2 = read_pkts(ssh_r2,1)
 
-  tmp1,tmp2 = local_read_pkts(2)
-  before_received +=tmp1;
-  before_dropped +=tmp2;
 
-  tmp1,tmp2 = local_read_pkts(3)
-  before_received +=tmp1;
-  before_dropped +=tmp2;
+  beforer22tmp1,beforer22tmp2 = read_pkts(ssh_r2,2)
+
+
+  beforer23tmp1,beforer23tmp2 = read_pkts(ssh_r2,3)
+
   
-  tmp1,tmp2 = local_read_pkts(4)
-  before_received +=tmp1;
-  before_dropped +=tmp2;
+#  tmp1,tmp2 = read_pkts(ssh_r2,4)
 
-  tmp1,tmp2 = local_read_pkts(5)
-  before_received +=tmp1;
-  before_dropped +=tmp2;
+#  before_received +=tmp1;
+#  before_dropped +=tmp2;
 
-  tmp1,tmp2 = local_read_pkts(6)
-  before_received +=tmp1;
-  before_dropped +=tmp2;
+#  tmp1,tmp2 = read_pkts(ssh_r2,5)
+#  before_received +=tmp1;
+#  before_dropped +=tmp2;
+
+#  tmp1,tmp2 = read_pkts(ssh_r2,6)
+#  before_received +=tmp1;
+#  before_dropped +=tmp2;
+
+  beforer31tmp1,beforer31tmp2 = read_pkts(ssh_r3,1)
 
 
+  beforer32tmp1,beforer32tmp2 = read_pkts(ssh_r3,2)
+
+
+  beforer33tmp1,beforer33tmp2 = read_pkts(ssh_r3,3)
+
+
+#  tmp1,tmp2 = read_pkts(ssh_r3,4)
+#  before_received +=tmp1;
+#  before_dropped +=tmp2;
+
+#  tmp1,tmp2 = read_pkts(ssh_r3,5)
+#  before_received +=tmp1;
+#  before_dropped +=tmp2;
+
+#  tmp1,tmp2 = read_pkts(ssh_r3,6)
+#  before_received +=tmp1;
+#  before_dropped +=tmp2;
   
   before_time = time.time() * 1000
 
   time.sleep(3)
 
-  tmp1,tmp2 = local_read_pkts(1)
-  after_received +=tmp1;
-  after_dropped +=tmp2;
+  afterr21tmp1,afterr21tmp2 = read_pkts(ssh_r2,1)
 
-  tmp1,tmp2 = local_read_pkts(2)
-  after_received +=tmp1;
-  after_dropped +=tmp2;
 
-  tmp1,tmp2 = local_read_pkts(3)
-  after_received +=tmp1;
-  after_dropped +=tmp2;
+  afterr22tmp1,afterr22tmp2 = read_pkts(ssh_r2,2)
+
+
+  afterr23tmp1,afterr23tmp2 = read_pkts(ssh_r2,3)
+
+#  tmp1,tmp2 = read_pkts(ssh_r2,4)
+#  after_received +=tmp1;
+#  after_dropped +=tmp2;
+
+#  tmp1,tmp2 = read_pkts(ssh_r2,5)
+#  after_received +=tmp1;
+#  after_dropped +=tmp2;
+
+#  tmp1,tmp2 = read_pkts(ssh_r2,6)
+#  after_received +=tmp1;
+#  after_dropped +=tmp2;
   
-  tmp1,tmp2 = local_read_pkts(4)
-  after_received +=tmp1;
-  after_dropped +=tmp2;
+  afterr31tmp1,afterr31tmp2 = read_pkts(ssh_r3,1)
 
-  tmp1,tmp2 = local_read_pkts(5)
-  after_received +=tmp1;
-  after_dropped +=tmp2;
 
-  tmp1,tmp2 = local_read_pkts(6)
-  after_received +=tmp1;
-  after_dropped +=tmp2;
+  afterr32tmp1,afterr32tmp2 = read_pkts(ssh_r3,2)
 
+
+  afterr33tmp1,afterr33tmp2 = read_pkts(ssh_r3,3)
+  
+  
+  print "r21throughput:"+str((afterr21tmp1-beforer21tmp1)/3)
+  print "r22throughput:"+str((afterr22tmp1-beforer22tmp1)/3)
+  print "r23throughput:"+str((afterr23tmp1-beforer23tmp1)/3)
+  print "r31throughput:"+str((afterr31tmp1-beforer31tmp1)/3)
+  print "r32throughput:"+str((afterr32tmp1-beforer32tmp1)/3)
+  print "r33throughput:"+str((afterr33tmp1-beforer33tmp1)/3)
+  print "r21drop:"+str(afterr21tmp2-beforer21tmp2)
+  print "r22drop:"+str(afterr22tmp2-beforer22tmp2)
+  print "r23drop:"+str(afterr23tmp2-beforer23tmp2)
+  print "r31drop:"+str(afterr31tmp2-beforer31tmp2)
+  print "r32drop:"+str(afterr32tmp2-beforer32tmp2)
+  print "r33drop:"+str(afterr33tmp2-beforer33tmp2)
+#  tmp1,tmp2 = read_pkts(ssh_r3,4)
+#  after_received +=tmp1;
+#  after_dropped +=tmp2;
+
+#  tmp1,tmp2 = read_pkts(ssh_r3,5)
+#  after_received +=tmp1;
+#  after_dropped +=tmp2;
+
+#  tmp1,tmp2 = read_pkts(ssh_r3,6)
+#  after_received +=tmp1;
+#  after_dropped +=tmp2;
 
   after_time = time.time() * 1000
 
+ # print "Stop testing..."
+  ssh_r2.close()
+  ssh_r3.close()
+  #stop_traffic_gen(options)
+  time.sleep(1)
   return (after_received-before_received)/3, after_dropped-before_dropped, after_time-before_time
+
+
 
 def main():
 
@@ -258,7 +320,7 @@ def main():
   for i in range(50):
     packet_out, packet_dropped, duration_time = test()
 #    local_packet_out, local_packet_dropped, local_duration_time = local_test()
-    print str(packet_out)
+    print "totalthroughput:"+str(packet_out)
 #    print str(local_packet_out)
     throughput.append(packet_out)
 #    flowgen.append(local_packet_out)
